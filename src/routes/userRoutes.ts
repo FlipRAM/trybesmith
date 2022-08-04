6d465df65046489016c125e4cf40cdb944b3e767
Felipe Rangel <feliperangel277@gmail.com>
2022-08-04 11:12:13 -0300

    Create validations for endpoint POST /users

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express';
 import UserController from '../controllers/userController';
+import UserMiddleware from '../middlewares/userMiddleware';
 
 const router = Router();
 
 const userController = new UserController();
+const userMiddleware = new UserMiddleware();
 
-router.post('/', userController.add);
+router.post(
+  '/',
+  userMiddleware.checkUsername,
+  userMiddleware.checkClasse,
+  userMiddleware.checkLevel,
+  userMiddleware.checkPassword,
+  userController.add,
+);
 
 export default router;
